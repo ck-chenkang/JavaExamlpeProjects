166515e2bdbf0860dbae6e1949c07a7ada092fd0
ck-chenkang <bettermanchenkang@163.com>
2021-07-26 20:13:12 +0800

    2021-7-26

--- Basic/src/com/ck/demo/MyCompute.java
+++
@@ -1,9 +0,0 @@
-package com.ck.demo;
-
-//自己写一个实现加减乘除的计算器
-public class MyCompute {
-    public static void main(String[] args) {
-        System.out.println("hello world!");
-    }
-
-}
--- Test/src/com/ck/main/Learning.java
+++ Test/src/com/ck/main/Learning.java
@@ -1,5 +1,6 @@
 package com.ck.main;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,13 +14,35 @@ import java.util.List;
  * jdbcb变成
  * 单元测试
  * 设计模式
+ * 注解
+ *
  */
 public class Learning {
     public static void main(String[] args) {
+        DataOutputStream out = null;
+        try {
+            File file = new File("file.txt");
+            out = new DataOutputStream(new FileOutputStream(file));
 
-    List<Integer> list = new ArrayList<Integer>();
-
-
+            out.writeBoolean(true);
+            out.writeByte((byte)0x41);
+            out.writeChar((char)0x4243);
+            out.writeShort((short)0x4445);
+            out.writeInt(0x12345678);
+            out.writeLong(0x0FEDCBA987654321L);
+            out.writeUTF("abcdefg");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch(IOException e) {
+            }
+        }
 
     }
 }
