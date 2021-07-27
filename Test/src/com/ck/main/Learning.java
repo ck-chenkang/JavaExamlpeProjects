71c01b64952afe79589f255e37d1798988d64798
ck-chenkang <bettermanchenkang@163.com>
2021-07-27 23:08:33 +0800

    增加多线程

--- Test/src/com/ck/main/Learning.java
+++ Test/src/com/ck/main/Learning.java
@@ -1,12 +1,21 @@
 package com.ck.main;
 
+import javax.xml.crypto.Data;
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
  * 教程链接：https://www.liaoxuefeng.com/wiki/1252599548343744
  * Java IO
+ * 线程池
+ * 线程同步
+ * 别人的jar包，怎么用，创建lib目录，add as library
+ * 静态代理，动态代理
+ * lambda表达式
+ * 静态内部类 局部内部类 匿名内部类，还有一个？
  * 范型
  * 加密与安全
  * maven基础
@@ -15,34 +24,23 @@ import java.util.List;
  * 单元测试
  * 设计模式
  * 注解
- *
+ * date时间、日期
  */
 public class Learning {
     public static void main(String[] args) {
-        DataOutputStream out = null;
-        try {
-            File file = new File("file.txt");
-            out = new DataOutputStream(new FileOutputStream(file));
-
-            out.writeBoolean(true);
-            out.writeByte((byte)0x41);
-            out.writeChar((char)0x4243);
-            out.writeShort((short)0x4445);
-            out.writeInt(0x12345678);
-            out.writeLong(0x0FEDCBA987654321L);
-            out.writeUTF("abcdefg");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                out.close();
-            } catch(IOException e) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println(new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date()));
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
-
+        }).start();
     }
 }
+
