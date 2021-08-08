9a3dd11402ea67acdde962268d6c18cffb2e2e8f
ck-chenkang <bettermanchenkang@163.com>
2021-08-08 10:11:25 +0800

    增加开发手册

--- Test/src/com/ck/main/Learning.java
+++ Test/src/com/ck/main/Learning.java
@@ -15,7 +15,8 @@ import java.util.List;
  * date时间、日期
  * 函数式编程，函数式接口
  * tomcat：下载、配置、目录结构
- *
+ * http：请求行、请求体、请求头、状态，不同开头的状态码的意思
+ * 发送一次http请求，经历了什么 https://blog.csdn.net/weixin_42307490/article/details/93910744
  */
 public class Learning {
 
