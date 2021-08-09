fe66c23ccd9c2c0ded26ae185a638d29cde1bbc0
ck-chenkang <bettermanchenkang@163.com>
2021-08-09 23:31:13 +0800

    maven

--- Test/src/com/ck/main/Learning.java
+++ Test/src/com/ck/main/Learning.java
@@ -14,10 +14,13 @@ import java.util.List;
  * 设计模式
  * date时间、日期
  * 函数式编程，函数式接口
- * tomcat：下载、配置、目录结构
+ * tomcat：下载、配置、目录结构、idea配置tomcat并使用
  * http：请求行、请求体、请求头、状态，不同开头的状态码的意思
  * 发送一次http请求，经历了什么 https://blog.csdn.net/weixin_42307490/article/details/93910744
  * maven下载、安装、系统环境变量配置、mirror配置、本地仓库
+ * idea创建maven工程（需要注意需要填写的gav的标准）、idea中maven设置、maven项目目录结构
+ * idea中maven侧边栏、pom.xml文件、gav、properties、dependencies
+ *
  *
  */
 public class Learning {
