6a15dfe89982ec4679cfeaa3b4ed9e983f8c7529
ck-chenkang <bettermanchenkang@163.com>
2021-08-13 11:28:22 +0800

    修改learning文件

--- Test/src/com/ck/main/Learning.java
+++ Test/src/com/ck/main/Learning.java
@@ -12,12 +12,8 @@ import java.util.List;
  * 单元测试
  * 设计模式
  * date时间、日期
- * 函数式编程，函数式接口
- * http：请求行、请求体、请求头、状态，不同开头的状态码的意思
- * 发送一次http请求，经历了什么 https://blog.csdn.net/weixin_42307490/article/details/93910744
  * idea中servelet hello world项目、Tomcat配置
  * servlet原理
- *
  *
  */
 public class Learning {
